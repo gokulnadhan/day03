5a4dfcde8d6665df815bbab91818c3b9ed4a8802
gokulnadhan <gokulnadhank1999@gmail.com>
2023-01-18 18:43:42 +0530

    secondcommit

---
+++ script.js
@@ -0,0 +1,6 @@
+var resume = {
+"Name":"Gokulnadhan.K"
+"Address":"61/197, EPB Nagar,Soolai, Veerappanchatram (PO), Erode-638004"
+"objective":"Secure a responsible career opportunity to fully utilize my training and skills, while making a significant contribution to the success of the company"
+"experience":"one year experience in wiring harness design at Yazaki India Pvt Limited, Maraimalainagar, Chengalpattu"
+}
